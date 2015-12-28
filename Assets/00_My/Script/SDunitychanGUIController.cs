c387b924c6325806f7762203f3888fa0ed7c764c
takahiro-kosaka <takahiro.kosaka@dena.com>
2015-12-28 23:58:26 +0900

    GUIの制御とかキャプチャスクリプトとかもろもろ追加

---
+++ Assets/00_My/Script/SDunitychanGUIController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityChan;
+public class SDunitychanGUIController : MonoBehaviour {
+
+	// SDユニティちゃんオブジェクト
+	private GameObject 	mUnityChanObject = null;
+
+	// GUIを表示するか否か
+	private bool		mEnableGUI = false;
+
+	// Use this for initialization
+	void Start () {
+		GameObject  parent 	=  transform.parent.gameObject;
+		mUnityChanObject 	= parent.transform.FindChild("SD_unitychan_humanoid_st").gameObject;
+		Debug.Assert (mUnityChanObject != null, "missing unity chan!");
+
+		SetGUIEnabled (mEnableGUI);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)){
+			mEnableGUI = !mEnableGUI;
+		}
+		SetGUIEnabled (mEnableGUI);
+	}
+
+	private void SetGUIEnabled (bool enabled) {
+		if (mUnityChanObject) {
+			mUnityChanObject.GetComponent<FaceUpdate> ().enabled 	= enabled;
+			mUnityChanObject.GetComponent<IdleChanger> ().enabled	= enabled;
+			mUnityChanObject.GetComponent<RandomWind> ().enabled 	= enabled;
+		}
+	}
+}
---
+++ Assets/00_My/Script/SunSimulator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunSimulator : MonoBehaviour {
+	public float mSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = transform.rotation * Quaternion.Euler (mSpeed * Time.deltaTime, 0, 0);
+	}
+}
